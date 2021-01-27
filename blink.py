b401685acc3550d78917a17398f5de78bc3f16a7
jdfeher <42353266+jdfeher@users.noreply.github.com>
2021-01-27 12:50:30 -0600

    Add files via upload
    Updated code from module

---
+++ blink.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+from time import sleep     # Import the sleep from time module
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+
+ITER_COUNT = 15  
+pin1 = 11
+
+GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)   
+
+hile ITER_COUNT > 0: # Run ITER_COUNT times
+   ITER_COUNT -= 1 # Decrement counter
+   GPIO.output(pin1, GPIO.HIGH) # Turn on
+   sleep(1)                     # Sleep for 1 second
+   GPIO.output(pin1, GPIO.LOW)  # Turn off
+   sleep(1)                     # Sleep for 1 second
+GPIO.cleanup()
---
+++ blink2.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+import time   
+import sys
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+
+ITER_COUNT = 15
+DEBUG = False
+if '-debug' in sys.argv:
+   DEBUG = True  
+LED_PIN = 11
+LED_IS_ON = False
+GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)   
+with open('data.txt', 'w') as data:
+    for i in range(0,ITER_COUNT): # loop through ITER_COUNT times
+        GPIO.output(LED_PIN, LED_IS_ON)
+        data.write(f'{time.time()*1000:1.0f} {LED_IS_ON}\n')
+        if DEBUG:
+            print(f'LED is on: {LED_IS_ON}')
+        LED_IS_ON = not(LED_IS_ON)
+        time.sleep(1)
+GPIO.cleanup() 
