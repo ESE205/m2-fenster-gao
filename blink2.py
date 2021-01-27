c53b1cf83ea6382e25ad680fb8b928af561f6a3d
jdfeher <42353266+jdfeher@users.noreply.github.com>
2021-01-27 13:50:47 -0600

    Update blink2.py
    Just show time in seconds

--- blink2.py
+++ blink2.py
@@ -14,7 +14,7 @@ GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 with open('data.txt', 'w') as data:
     for i in range(0,ITER_COUNT): # loop through ITER_COUNT times
         GPIO.output(LED_PIN, LED_IS_ON)
-        data.write(f'{time.time()*1000:1.0f} {LED_IS_ON}\n')
+        data.write(f'{time.time():1.0f} {LED_IS_ON}\n')
         if DEBUG:
             print(f'LED is on: {LED_IS_ON}')
         LED_IS_ON = not(LED_IS_ON)
